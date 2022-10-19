e150c190b24e9516280ea741862cfae664099d61
Theeradech Dechasit <notegamer@hotmail.com>
2022-10-20 00:35:30 +0700

    add npm start

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -17,11 +19,33 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller}/{action=Index}/{id?}"
+    );
+});
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
+if (app.Environment.IsDevelopment())
+{
 
-app.MapFallbackToFile("index.html");;
+    var processStartInfo = new ProcessStartInfo();
+    processStartInfo.WorkingDirectory = @"./ClientApp";
+    processStartInfo.FileName = "npm";
+    processStartInfo.Arguments = "run dev";
+    Process.Start(processStartInfo);
+
+    Thread.Sleep(2000);
+
+    app.UseSpa(spa =>
+    {
+        spa.UseProxyToSpaDevelopmentServer("http://localhost:3000");
+    });
+}
+else
+{
+    app.MapFallbackToFile("index.html");
+}
 
 app.Run();
